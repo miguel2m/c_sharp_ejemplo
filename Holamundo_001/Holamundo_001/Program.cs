6fde6f892806f222ec4b2b9c38ff2969cf33e042
miguel2m <miguelangelmedina2494@gmail.com>
2017-10-16 09:05:05 -0400

    Creando proyecto en Visual Studio

---
+++ Holamundo_001/Holamundo_001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Holamundo_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+        }
+    }
+}
